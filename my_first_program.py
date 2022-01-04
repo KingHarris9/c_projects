2c1728d30bd06853e7982c8f7f75b62a24eb2ac7
KingHarris9 <96508243+KingHarris9@users.noreply.github.com>
2022-01-03 22:07:58 -0800

    Add files via upload

---
+++ my_first_program.py
@@ -0,0 +1,23 @@
+# Write a program that finds the area of a rectangle
+"""
+side1 = 20
+side2 = 30
+areaRect = side1 * side2
+print("The area of your rectangle is", areaRect)
+"""
+
+# Write a program that uses two words, such as "Hello" and "World" and stitches
+# (concatenates) them together to print them out.
+"""
+word1 = "Hello"
+word2 = "World"
+print(word1 + " " + word2)
+print("Hello " + "World")
+print("Hello" + " " + "World")
+"""
+# Divide two numbers (e.g. 5/2) and print an integer
+num1 = 5
+num2 = 2
+# Takes the floating point result and converts it to an integer
+print("The integer value of your division is", int(num1/num2))
+
